ce21c36f9a224e8eeb015b35994584ca745579ae
Jing Ren <jingren@192.168.1.190>
2023-04-19 17:22:12 +0100

    add models

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,11 @@
-from flask import Flask, Request, request, render_template
+import numpy as np
+from flask import Flask, Request, request
 from werkzeug.datastructures import ImmutableOrderedMultiDict
-from predict import predict
+from predict import predict_nn, predict_xgb
 import pandas as pd
+from flasgger import Swagger
+from flasgger import swag_from
+import logging
 
 
 class OrderedRequest(Request):
@@ -11,27 +15,171 @@ class OrderedRequest(Request):
 class MyFlask(Flask):
     request_class = OrderedRequest
 
-
 app = MyFlask(__name__, static_folder="public", template_folder="views")
 
+swagger_template = dict(
+info = {
+    'title': 'LendWise Loan Risk Prediction APIs',
+    'version': '0.1',
+    'description': 'APIs for predicting the recovery rate of a loan based on various input parameters.',
+    }
+)
 
-@app.route("/")
-def homepage():
-    return render_template("index.html")
+# Initialize Swagger with the desired configuration options
+swagger_config = {
+    "specs": [
+            {
+                "endpoint": "lendwise",
+                "route": "/lendwise",
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
+            }
+        ],
+    "headers": [],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/",
+}
+swagger = Swagger(app, template=swagger_template, config=swagger_config)
 
+logger = logging.getLogger(__name__)
 
-@app.route("/api/predict")
-def loan_risk_predict():
+@app.route("/api/predict/nn")
+@swag_from("./swagger/nn.yml", methods=['GET'])
+def loan_risk_predict_nn():
     assert isinstance(request.args, ImmutableOrderedMultiDict)
 
-    prediction = predict(request.args)
+    nn_model_list = [
+        "./model/nn_loan_risk_model_0",
+        "./model/nn_loan_risk_model_1",
+        "./model/nn_loan_risk_model_2",
+        "./model/nn_loan_risk_model_3",
+        "./model/nn_loan_risk_model_4",
+    ]
+    data_transformer = "./model/nn_data_transformer.joblib"
+
+    # Make predictions using each model
+    predictions = []
+    for i in range(len(nn_model_list)):
+        pred = predict_nn(request.args, data_transformer, i, nn_model_list[i])
+        predictions.append(pred)
+
+    # Average the predictions across all models
+    prediction = np.mean(predictions)
 
     if pd.isna(prediction):
+        logger.error(f"Prediction is not a valid number: {prediction}")
+        logger.error(f"Input data: {request.args}")
         return {"error": "There's something wrong with your input."}, 400
 
     if prediction < 0:
+        logger.warning(f"Predicted value {prediction} is less than 0, setting to 0")
         prediction = 0
     elif prediction > 1:
+        logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
+        prediction = 1
+
+    description = f"This loan is predicted to recover {round(prediction * 100, 1)}% of its expected return."
+
+    return {"value": str(prediction), "description": description}
+
+@app.route("/api/predict/xgb")
+@swag_from("./swagger/xgb.yml", methods=['GET'])
+def loan_risk_predict_xgb():
+    assert isinstance(request.args, ImmutableOrderedMultiDict)
+
+    xgb_model_list = [
+        "./model/xgb_loan_risk_model_0.json",
+        "./model/xgb_loan_risk_model_1.json",
+        "./model/xgb_loan_risk_model_2.json",
+        "./model/xgb_loan_risk_model_3.json",
+        "./model/xgb_loan_risk_model_4.json",
+    ]
+    data_transformer = "./model/xgb_data_transformer.joblib"
+
+    # Make predictions using each model
+    predictions = []
+    for i in range(len(xgb_model_list)):
+        pred = predict_xgb(request.args, data_transformer, i, xgb_model_list[i])
+        predictions.append(pred)
+
+    # Average the predictions across all models
+    prediction = np.mean(predictions)
+
+    if pd.isna(prediction):
+        logger.error(f"Prediction is not a valid number: {prediction}")
+        logger.error(f"Input data: {request.args}")
+        return {"error": "There's something wrong with your input."}, 400
+
+    if prediction < 0:
+        logger.warning(f"Predicted value {prediction} is less than 0, setting to 0")
+        prediction = 0
+    elif prediction > 1:
+        logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
+        prediction = 1
+
+    description = f"This loan is predicted to recover {round(prediction * 100, 1)}% of its expected return."
+
+    return {"value": str(prediction), "description": description}
+
+@app.route("/api/predict/ensemble")
+@swag_from("./swagger/xgb.yml", methods=['GET'])
+def loan_risk_predict_ensemble():
+    assert isinstance(request.args, ImmutableOrderedMultiDict)
+
+    nn_model_list = [
+        "./model/nn_loan_risk_model_0",
+        "./model/nn_loan_risk_model_1",
+        "./model/nn_loan_risk_model_2",
+        "./model/nn_loan_risk_model_3",
+        "./model/nn_loan_risk_model_4",
+    ]
+    nn_data_transformer = "./model/nn_data_transformer.joblib"
+
+    # Make predictions using each model
+    nn_predictions = []
+    for i in range(len(nn_model_list)):
+        pred = predict_nn(request.args, nn_data_transformer, i, nn_model_list[i])
+        nn_predictions.append(pred)
+
+    # Average the predictions across all models
+    prediction_nn = np.mean(nn_predictions)
+
+    xgb_model_list = [
+        "./model/xgb_loan_risk_model_0.json",
+        "./model/xgb_loan_risk_model_1.json",
+        "./model/xgb_loan_risk_model_2.json",
+        "./model/xgb_loan_risk_model_3.json",
+        "./model/xgb_loan_risk_model_4.json",
+    ]
+    xgb_data_transformer = "./model/xgb_data_transformer.joblib"
+
+    # Make predictions using each model
+    xgb_predictions = []
+    for i in range(len(xgb_model_list)):
+        pred = predict_xgb(request.args, xgb_data_transformer, i, xgb_model_list[i])
+        xgb_predictions.append(pred)
+
+    # Average the predictions across all models
+    prediction_xgb = np.mean(xgb_predictions)
+
+    # Define the weights for each model
+    xgb_weight = 0.6
+    nn_weight = 0.4
+
+    # Calculate the weighted average of the predictions
+    prediction = (xgb_weight * prediction_xgb) + (nn_weight * prediction_nn)
+
+    if pd.isna(prediction):
+        logger.error(f"Prediction is not a valid number: {prediction}")
+        logger.error(f"Input data: {request.args}")
+        return {"error": "There's something wrong with your input."}, 400
+
+    if prediction < 0:
+        logger.warning(f"Predicted value {prediction} is less than 0, setting to 0")
+        prediction = 0
+    elif prediction > 1:
+        logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
         prediction = 1
 
     description = f"This loan is predicted to recover {round(prediction * 100, 1)}% of its expected return."
--- predict.py
+++ predict.py
@@ -2,39 +2,73 @@ import pandas as pd
 import joblib
 from tensorflow.keras.models import load_model
 import numpy as np
+import logging
+import xgboost as xgb
 
 
-def predict(arguments):
+def dataPrepare(data):
+
+    for col_name in data:
+        data[col_name] = pd.to_numeric(data[col_name], errors="ignore")
+
+    inverse_columns = [col_name for col_name in data if "inv_" in col_name]
+
+    def invert(value):
+        if type(value) is str:
+            return 0
+        else:
+            return 1 / 1 if value == 0 else 1 / value
+
+    for col_name in inverse_columns:
+        data[col_name] = data[col_name].map(invert)
+
+    for joint_col, indiv_col in zip(
+            ["annual_inc_joint", "dti_joint"], ["annual_inc", "dti"]
+    ):
+        data[joint_col] = [
+            indiv_val if type(joint_val) is str else joint_val
+            for joint_val, indiv_val in zip(data[joint_col], data[indiv_col])
+        ]
+
+def predict_nn(arguments, transformer, index, model):
     try:
         data = pd.DataFrame(arguments, index=[0])
+        dataPrepare(data)
 
-        for col_name in data:
-            data[col_name] = pd.to_numeric(data[col_name], errors="ignore")
-
-        inverse_columns = [col_name for col_name in data if "inv_" in col_name]
-
-        def invert(value):
-            if type(value) is str:
-                return 0
-            else:
-                return 1 / 1 if value == 0 else 1 / value
-
-        for col_name in inverse_columns:
-            data[col_name] = data[col_name].map(invert)
-
-        for joint_col, indiv_col in zip(
-            ["annual_inc_joint", "dti_joint"], ["annual_inc", "dti"]
-        ):
-            data[joint_col] = [
-                indiv_val if type(joint_val) is str else joint_val
-                for joint_val, indiv_val in zip(data[joint_col], data[indiv_col])
-            ]
-
-        transformer = joblib.load("./models/data_transformer.joblib")
+        transformer = joblib.load(transformer)[index]
         X = transformer.transform(data)
 
-        model = load_model("./models/loan_risk_model")
-        return model(X).numpy()[0][0]
+        model = load_model(model)
 
-    except:
+        y_pred = model(X).numpy()[0][0]
+
+        logging.info("The prediction result is: ")
+        logging.info(y_pred)
+
+        return y_pred
+
+    except Exception as e:
+        logging.error(f"Error in prediction: {e}")
+        return np.nan
+
+
+def predict_xgb(arguments, transformer, index, model_link):
+    try:
+        data = pd.DataFrame(arguments, index=[0])
+        dataPrepare(data)
+
+        transformer = joblib.load(transformer)[index]
+        X = transformer.transform(data)
+
+        model = xgb.Booster()
+        model.load_model(model_link)
+
+        # Use the predict method on the loaded model object to obtain the predicted probabilities
+        y_pred = model.predict(xgb.DMatrix(X))[0]
+        logging.info("The prediction result is: ")
+        logging.info(y_pred)
+        return y_pred
+
+    except Exception as e:
+        logging.error(f"Error in prediction: {e}")
         return np.nan
