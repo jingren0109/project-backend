f7709bfd9799536d5d98f3757a5410ac4b677de9
Jing Ren <jingren@192.168.1.190>
2023-04-20 17:48:52 +0100

    add new api

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def loan_risk_predict_nn():
         logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
         prediction = 1
 
-    description = f"This loan is predicted to recover {round(prediction * 100, 1)}% of its expected return."
+    description = f"This loan is predicted to recover {round(prediction * 100, 4)}% of its expected return."
 
     response = jsonify({"value": str(prediction), "description": description})
 
@@ -123,7 +123,7 @@ def loan_risk_predict_xgb():
         logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
         prediction = 1
 
-    description = f"This loan is predicted to recover {round(prediction * 100, 1)}% of its expected return."
+    description = f"This loan is predicted to recover {round(prediction * 100, 4)}% of its expected return."
 
     response = jsonify({"value": str(prediction), "description": description})
 
@@ -155,6 +155,13 @@ def loan_risk_predict_ensemble():
     # Average the predictions across all models
     prediction_nn = np.mean(nn_predictions)
 
+    if prediction_nn < 0:
+        logger.warning(f"Predicted value {prediction_nn} is less than 0, setting to 0")
+        prediction_nn = 0
+    elif prediction_nn > 1:
+        logger.warning(f"Predicted value {prediction_nn} is greater than 1, setting to 1")
+        prediction_nn = 1
+
     xgb_model_list = [
         "./model/xgb_loan_risk_model_0.json",
         "./model/xgb_loan_risk_model_1.json",
@@ -172,6 +179,13 @@ def loan_risk_predict_ensemble():
 
     # Average the predictions across all models
     prediction_xgb = np.mean(xgb_predictions)
+
+    if prediction_xgb < 0:
+        logger.warning(f"Predicted value {prediction_xgb} is less than 0, setting to 0")
+        prediction_xgb = 0
+    elif prediction_xgb > 1:
+        logger.warning(f"Predicted value {prediction_xgb} is greater than 1, setting to 1")
+        prediction_xgb = 1
 
     # Define the weights for each model
     xgb_weight = 0.6
@@ -192,9 +206,95 @@ def loan_risk_predict_ensemble():
         logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
         prediction = 1
 
-    description = f"This loan is predicted to recover {round(prediction * 100, 1)}% of its expected return."
+    description = f"This loan is predicted to recover {round(prediction * 100, 4)}% of its expected return."
 
     response = jsonify({"value": str(prediction), "description": description})
+
+    # Add the following line to allow any origin to access the resource
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route("/api/predict")
+def loan_risk_predict():
+    assert isinstance(request.args, ImmutableOrderedMultiDict)
+
+    nn_model_list = [
+        "./model/nn_loan_risk_model_0",
+        "./model/nn_loan_risk_model_1",
+        "./model/nn_loan_risk_model_2",
+        "./model/nn_loan_risk_model_3",
+        "./model/nn_loan_risk_model_4",
+    ]
+    nn_data_transformer = "./model/nn_data_transformer.joblib"
+
+    # Make predictions using each model
+    nn_predictions = []
+    for i in range(len(nn_model_list)):
+        pred = predict_nn(request.args, nn_data_transformer, i, nn_model_list[i])
+        nn_predictions.append(pred)
+
+    # Average the predictions across all models
+    prediction_nn = np.mean(nn_predictions)
+
+    if prediction_nn < 0:
+        logger.warning(f"Predicted value {prediction_nn} is less than 0, setting to 0")
+        prediction_nn = 0
+    elif prediction_nn > 1:
+        logger.warning(f"Predicted value {prediction_nn} is greater than 1, setting to 1")
+        prediction_nn = 1
+
+    xgb_model_list = [
+        "./model/xgb_loan_risk_model_0.json",
+        "./model/xgb_loan_risk_model_1.json",
+        "./model/xgb_loan_risk_model_2.json",
+        "./model/xgb_loan_risk_model_3.json",
+        "./model/xgb_loan_risk_model_4.json",
+    ]
+    xgb_data_transformer = "./model/xgb_data_transformer.joblib"
+
+    # Make predictions using each model
+    xgb_predictions = []
+    for i in range(len(xgb_model_list)):
+        pred = predict_xgb(request.args, xgb_data_transformer, i, xgb_model_list[i])
+        xgb_predictions.append(pred)
+
+    # Average the predictions across all models
+    prediction_xgb = np.mean(xgb_predictions)
+
+    if prediction_xgb < 0:
+        logger.warning(f"Predicted value {prediction_xgb} is less than 0, setting to 0")
+        prediction_xgb = 0
+    elif prediction_xgb > 1:
+        logger.warning(f"Predicted value {prediction_xgb} is greater than 1, setting to 1")
+        prediction_xgb = 1
+
+    # Define the weights for each model
+    xgb_weight = 0.6
+    nn_weight = 0.4
+
+    # Calculate the weighted average of the predictions
+    prediction = (xgb_weight * prediction_xgb) + (nn_weight * prediction_nn)
+
+    if pd.isna(prediction):
+        logger.error(f"Prediction is not a valid number: {prediction}")
+        logger.error(f"Input data: {request.args}")
+        return {"error": "There's something wrong with your input."}, 400
+
+    if prediction < 0:
+        logger.warning(f"Predicted value {prediction} is less than 0, setting to 0")
+        prediction = 0
+    elif prediction > 1:
+        logger.warning(f"Predicted value {prediction} is greater than 1, setting to 1")
+        prediction = 1
+
+    nn_description = f"This loan is predicted to recover {round(prediction_nn * 100, 4)}% of its expected return."
+    xgb_description = f"This loan is predicted to recover {round(prediction_xgb * 100, 4)}% of its expected return."
+    description = f"This loan is predicted to recover {round(prediction * 100, 4)}% of its expected return."
+
+    response = jsonify({"nn": {"value": str(prediction_nn), "description": nn_description},
+                        "xgb": {"value": str(prediction_xgb), "description": xgb_description},
+                        "ensemble": {"value": str(prediction), "description": description}})
 
     # Add the following line to allow any origin to access the resource
     response.headers.add('Access-Control-Allow-Origin', '*')
